600b356fb779300d31aa66d058f79851cbc94fdb
Dmitry Zagorevskiy <7281015@gmail.com>
2022-01-07 15:30:26 +0100

    Fixed find_bad_words.py added lower

--- gcc_app/utils/find_bad_words.py
+++ gcc_app/utils/find_bad_words.py
@@ -6,6 +6,7 @@ from app import BAD_WORDS
 
 async def find_bad_words(text: str, bad_words: frozenset = BAD_WORDS) -> FrozenSet:
     # remove punctuation marks or ( text = re.sub(r'[^\w\s]','',text) )
+    text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = frozenset(text.strip().split(" "))
     bad_words = text.intersection(BAD_WORDS)
